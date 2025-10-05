a7bf403d80561609279a997a3a1883a96626caa7
metehanalperelmas <elmasmetehanalper@gmail.com>
2025-10-05 15:39:45 +0300

    java date and time

---
+++ src/easy/JavaDateAndTime.java
@@ -0,0 +1,52 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        java.time.LocalDate date = java.time.LocalDate.of(year, month, day);
+        return date.getDayOfWeek().toString();
+    }
+
+}
+public class JavaDateAndTime {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        System.out.println(res);
+
+        bufferedReader.close();
+    }
+
+}
