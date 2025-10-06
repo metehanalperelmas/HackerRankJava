f464794e9b6df757ad722611c10def1e11a29aae
metehanalperelmas <elmasmetehanalper@gmail.com>
2025-10-06 09:41:17 +0300

    java string introduction

---
+++ src/easy/JavaStringsIntroduction.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.Scanner;
+
+public class JavaStringsIntroduction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        sc.close();
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(Character.toUpperCase(A.charAt(0)) + A.substring(1) + " " + Character.toUpperCase(B.charAt(0)) + B.substring(1));
+    }
+}
