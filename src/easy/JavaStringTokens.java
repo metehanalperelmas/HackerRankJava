2f366d7eca17fb8eb855cfc09509f3d8b4ba88d4
metehanalperelmas <elmasmetehanalper@gmail.com>
2025-10-21 14:52:40 +0300

    java string tokens

---
+++ src/easy/JavaStringTokens.java
@@ -0,0 +1,21 @@
+package easy;
+
+import java.util.Scanner;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        if (s.trim().isEmpty()) {
+            System.out.println(0);
+        } else {
+            System.out.println(s.trim().split("[!,?. @_']+").length);
+            for (String a : s.trim().split("[!,?. @_']+")) {
+                System.out.println(a);
+            }
+        }
+        scan.close();
+    }
+
+}
